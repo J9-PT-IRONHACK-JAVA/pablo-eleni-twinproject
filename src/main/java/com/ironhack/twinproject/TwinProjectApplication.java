96b2bf64e6c5854fcbbdf65aba4b29d557c16f78
eleni <eleni@yukisoftware.es>
2022-12-15 18:00:21 +0100

    Added menu for categories

--- src/main/java/com/ironhack/twinproject/TwinProjectApplication.java
+++ src/main/java/com/ironhack/twinproject/TwinProjectApplication.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 @RequiredArgsConstructor
 public class TwinProjectApplication implements CommandLineRunner {
