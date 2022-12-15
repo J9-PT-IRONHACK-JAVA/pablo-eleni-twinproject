abf7405bec372fedf4b5c5d43f8287738ac6c6e1
eleni <eleni@yukisoftware.es>
2022-12-15 17:15:58 +0100

    Added menu for categories

--- src/main/java/com/ironhack/twinproject/TwinProjectApplication.java
+++ src/main/java/com/ironhack/twinproject/TwinProjectApplication.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @RequiredArgsConstructor
